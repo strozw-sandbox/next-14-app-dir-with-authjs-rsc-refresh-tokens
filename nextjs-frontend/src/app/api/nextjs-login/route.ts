093ce1bb047ff7f8f7928d4f9c8690949616f747
strozw <strozw@users.noreply.github.com>
2024-06-07 09:29:45 +0900

    refactor: convert to monorepo & update next.js & add biome

--- nextjs-frontend/src/app/api/nextjs-login/route.ts
+++ nextjs-frontend/src/app/api/nextjs-login/route.ts
@@ -1,52 +1,66 @@
-import jwtDecode from 'jwt-decode';
-import { NextRequest, NextResponse } from 'next/server';
-import { cookies } from 'next/headers';
-import { Session } from '@/app/types';
-import { encryptServerSession } from '@/app/utils';
+import { jwtDecode } from "jwt-decode";
+import { cookies } from "next/headers";
+import type { Session } from "@/app/types";
+import { encryptServerSession } from "@/app/utils";
+import { NextResponse, type NextRequest } from "next/server";
 
 export const POST = async (request: NextRequest) => {
-  let data;
+	// biome-ignore lint/suspicious/noImplicitAnyLet: <explanation>
+	let data;
 
-  try {
-    data = await request.json();
+	try {
+		data = await request.json();
 
-    const fetchResponse = await fetch('http://localhost:8000/express-api/auth/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    });
+		const fetchResponse = await fetch(
+			"http://localhost:8000/express-api/auth/login",
+			{
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(data),
+			},
+		);
 
-    if (!fetchResponse.ok) {
-      const error = await fetchResponse.json();
-      return NextResponse.json({ message: error.message }, { status: fetchResponse.status });
-    }
+		if (!fetchResponse.ok) {
+			const error = await fetchResponse.json();
+			return NextResponse.json(
+				{ message: error.message },
+				{ status: fetchResponse.status },
+			);
+		}
 
-    const response = await fetchResponse.json();
+		const response = await fetchResponse.json();
 
-    const accessTokenPayload = jwtDecode<any>(response.accessToken);
-    const refreshTokenPayload = jwtDecode<any>(response.refreshToken);
+		const accessTokenPayload = jwtDecode<{ user: Session["user"] }>(
+			response.accessToken,
+		);
+		const refreshTokenPayload = jwtDecode<{ exp: number }>(
+			response.refreshToken,
+		);
 
-    const session: Session = {
-      accessToken: response.accessToken,
-      refreshToken: response.refreshToken,
-      user: accessTokenPayload.user,
-    };
+		const session: Session = {
+			accessToken: response.accessToken,
+			refreshToken: response.refreshToken,
+			user: accessTokenPayload.user,
+		};
 
-    const encryptedServerSession = encryptServerSession(session);
+		const encryptedServerSession = encryptServerSession(session);
 
-    cookies().set({
-      name: 'session',
-      value: encryptedServerSession,
-      httpOnly: true,
-      expires: new Date(refreshTokenPayload.exp * 1000),
-      sameSite: 'lax',
-      secure: true,
-    });
+		cookies().set({
+			name: "session",
+			value: encryptedServerSession,
+			httpOnly: true,
+			expires: new Date(refreshTokenPayload.exp * 1000),
+			sameSite: "lax",
+			secure: true,
+		});
 
-    return NextResponse.json(session, { status: 200 });
-  } catch (e: unknown) {
-    return NextResponse.json({ message: 'An unknown error occurred.' }, { status: 500 });
-  }
+		return NextResponse.json(session, { status: 200 });
+	} catch (e: unknown) {
+		return NextResponse.json(
+			{ message: "An unknown error occurred." },
+			{ status: 500 },
+		);
+	}
 };
